07bdea3b35f8fa821e9f7ce01e03344a6313cfb5
Michael Rutter <michael.rutter@protonmail.com>
2022-10-09 01:53:13 +0100

    made config static

--- todone-backend/src/config.rs
+++ todone-backend/src/config.rs
@@ -1,7 +1,15 @@
 use clap::Parser;
+use once_cell::sync::Lazy;
+
+pub static CONFIG: Lazy<Config> = Lazy::new(|| {
+    dotenv::dotenv().ok();
+    Config::parse()
+});
 
 #[derive(Parser, Debug)]
 pub struct Config {
     #[arg(env = "DATABASE_URL")]
     pub database_url: String,
+    #[arg(env = "JWT_SECRET")]
+    pub jwt_secret: String,
 }
--- todone-backend/src/main.rs
+++ todone-backend/src/main.rs
@@ -1,17 +1,12 @@
 use std::net::SocketAddr;
 
-use clap::Parser;
 use sqlx::postgres::PgPoolOptions;
 
-use todone_backend::{config::Config, endpoints};
+use todone_backend::{config::CONFIG, endpoints};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenv::dotenv().ok();
-
-    let config: Config = Config::parse();
-
-    let db_pool = PgPoolOptions::new().connect(&config.database_url).await?;
+    let db_pool = PgPoolOptions::new().connect(&CONFIG.database_url).await?;
 
     let app = endpoints::app(db_pool);
 
