d6f83f92544d6453d3dd7bdf417acbcb803ef407
Michael Rutter <michael.rutter@protonmail.com>
2022-11-12 12:16:08 +0000

    migrated from log to tracing lib

--- todone-backend/src/main.rs
+++ todone-backend/src/main.rs
@@ -4,11 +4,22 @@ use sqlx::postgres::PgPoolOptions;
 
 use todone_backend::{config::CONFIG, endpoints};
 
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
 
     env_logger::init();
+
+    // Setting up logging
+    {
+        let subscriber = FmtSubscriber::builder()
+            .with_env_filter(EnvFilter::from_default_env())
+            .finish();
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("setting default subscriber failed");
+    }
 
     let db_pool = PgPoolOptions::new().connect(&CONFIG.database_url).await?;
 
